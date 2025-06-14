2ebaa72944613f3bd4ff778befa65fee780bb0d2
DSTBP <TaddeoWang123@gmail.com>
2025-06-14 19:16:53 +0800

    Update

--- utils/CSCSourceFormatter/assets/js/fillin_code.js
+++ utils/CSCSourceFormatter/assets/js/fillin_code.js
@@ -637,7 +637,9 @@ async function getCode() {
                             top: docx.convertInchesToTwip(parseFloat(currentFormData.margins.top) / 2.54),
                             bottom: docx.convertInchesToTwip(parseFloat(currentFormData.margins.bottom) / 2.54),
                             left: docx.convertInchesToTwip(parseFloat(currentFormData.margins.left) / 2.54),
-                            right: docx.convertInchesToTwip(parseFloat(currentFormData.margins.right) / 2.54)
+                            right: docx.convertInchesToTwip(parseFloat(currentFormData.margins.right) / 2.54),
+                            header: docx.convertInchesToTwip(parseFloat(formData.headerMarginTop) / 2.54),   // 页眉距顶边 1.5cm
+                            footer: docx.convertInchesToTwip(parseFloat(formData.footerMarginBottom) / 2.54)   // 页脚距底边 1.75cm
                         }
                     } : {
                         margin: {
